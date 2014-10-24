ab235633b9c7acbd6ad6ee6bc415c5b9d37283db
sansquer <simon.ansquer@gmail.com>
2014-10-24 18:32:15 +0200

    Création des signatures des méthodes du contrôleur des commentaires (CommentController)

--- ModuleBlog/ModuleBlog/Controllers/Models/Commentaire.cs
+++ ModuleBlog/ModuleBlog/Controllers/Models/Commentaire.cs
@@ -6,21 +6,19 @@ using System.Threading.Tasks;
 
 namespace ModuleBlog.Controllers.Models
 {
-    public class Article
+    public class Commentaire
     {
+        public int Commentaire_id { get; set; }
         public int Article_id { get; set; }
-        public int Blog_id { get; set; }
-        public string TitreArticle { get; set; }
-        public string ImageChemin { get; set; }
-        public string ContenuArticle { get; set; }
-        public int Evenement_id { get; set; }
+        public int Utilisateur_id { get; set; }
+        public string ContenuCommentaire { get; set; }
         public DateTime DateCreation { get; set; }
         public DateTime DateModification { get; set; }
         public bool Actif { get; set; }
 
-        public Article()
+        public Commentaire()
         {
-
+                
         }
     }
 }
