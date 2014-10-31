8cc0fb3e30662629bac152dbfe14e4a269985efd
sansquer <simon.ansquer@gmail.com>
2014-10-31 17:07:55 +0100

    [BLL][CATEGORIE] Test sur la couche BLL Catégorie

--- ModuleBlog/ModuleBlog.Tests/UnitTestCategorie.cs
+++ ModuleBlog/ModuleBlog.Tests/UnitTestCategorie.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModuleBlog.DAL;
 using System.Collections.Generic;
 using ModuleBlog.DAL.Models;
+using ModuleBlog.BLL.Models;
+using ModuleBlog.BLL;
 
 namespace ModuleBlog.Tests
 {
@@ -10,6 +12,7 @@ namespace ModuleBlog.Tests
     public class UnitTestCategorie
     {
         private CATEGORIE_DAL categoryDal;
+        private CATEGORIE_BLL categoryBll;
 
         [TestInitialize]
         public void Init()
@@ -17,13 +20,26 @@ namespace ModuleBlog.Tests
             categoryDal = new CATEGORIE_DAL();
             // on vérifie que l'instance n'est pas nulle
             Assert.IsNotNull(categoryDal);
+
+            categoryBll = new CATEGORIE_BLL();
+            Assert.IsNotNull(categoryBll);
         }
 
         [TestMethod]
-        public void TestLectureCategories()
+        public void TestLectureCategoriesDAL()
         {
             List<CategorieDao> categoryList;
             categoryList = categoryDal.GetCategories();
+
+            Assert.IsNotNull(categoryList);
+            Assert.IsTrue(categoryList.Count > 0, "Liste des catégories vide");
+        }
+
+        [TestMethod]
+        public void TestLectureCategoriesBLL()
+        {
+            List<CategorieBLL> categoryList;
+            categoryList = categoryBll.GetCategories();
 
             Assert.IsNotNull(categoryList);
             Assert.IsTrue(categoryList.Count > 0, "Liste des catégories vide");
