a4e3e8b35cfac10d2f5748f3ba8bd9122958fc36
sansquer <simon.ansquer@gmail.com>
2014-10-31 14:24:12 +0100

    [DAL][TESTS] [CATEGORIE] test unitaire pour la DAL Catégorie

---
+++ ModuleBlog/ModuleBlog.Tests/CATEGORIE_TESTS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModuleBlog.DAL;
+using System.Collections.Generic;
+using ModuleBlog.DAL.Models;
+
+namespace ModuleBlog.Tests
+{
+    [TestClass]
+    public class CATEGORIE_TESTS
+    {
+        private CATEGORIE_DAL categoryDal;
+
+        [TestInitialize]
+        public void Init()
+        {
+            categoryDal = new CATEGORIE_DAL();
+            // on vérifie que l'instance n'est pas nulle
+            Assert.IsNotNull(categoryDal);
+        }
+
+        [TestMethod]
+        public void TestLectureCategories()
+        {
+            List<CategorieDao> categoryList;
+            categoryList = categoryDal.GetCategories();
+
+            Assert.IsNotNull(categoryList);
+            Assert.IsTrue(categoryList.Count > 0, "Liste des catégories vide");
+        }
+    }
+}
