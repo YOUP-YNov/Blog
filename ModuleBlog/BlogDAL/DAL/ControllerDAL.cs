3815e7d46d4cf3e4a55ba35a8d82e4c2c6f47286
svirabouth <stephane.virabouth@gmail.com>
2014-11-20 14:08:52 +0100

    [Probleme Git]

---
+++ ModuleBlog/BlogDAL/DAL/ControllerDAL.cs
@@ -0,0 +1,48 @@
+﻿using Logger;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDAL.DAL
+{
+    public class ControllerDAL
+    {
+        internal SqlCommand cmd;
+        internal SqlConnection con;
+        internal SqlDataAdapter da;
+        internal DataSet ds;
+        internal string strcon = Connector.ConnectionString;
+        internal string loggerUrl = "http://loggerasp.azurewebsites.net/";
+
+        public ControllerDAL()
+        {
+            if(con == null)
+                con = new SqlConnection(strcon);
+
+            ds = new DataSet();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = con;
+        }
+
+        public void FillData(string psText, ref DataSet ds, Dictionary<string, object> listParameters = null)
+        {
+            cmd.CommandText = psText;
+            if (listParameters != null)
+                for (int i = 0; i < listParameters.Count; i++)
+                    cmd.Parameters.AddWithValue(listParameters.ElementAt(i).Key, listParameters.ElementAt(i).Value);
+            da = new SqlDataAdapter(cmd);
+            con.Open();
+            da.Fill(ds);
+        }
+
+        public void LogException(Exception ex, string path, string message, int priority)
+        {
+            new LErreur(ex, path, message, priority).Save(loggerUrl);
+        }
+    }
+}
