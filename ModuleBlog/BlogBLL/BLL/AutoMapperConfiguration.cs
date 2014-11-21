3d69641ff4933f1c1effb8128637989463375547
sansquer <simon.ansquer@gmail.com>
2014-11-21 14:15:59 +0100

    Ajout de commentaire sur la configuration des create map sur la BLL

--- ModuleBlog/BlogBLL/BLL/AutoMapperConfiguration.cs
+++ ModuleBlog/BlogBLL/BLL/AutoMapperConfiguration.cs
@@ -10,6 +10,9 @@ namespace ModuleBlog.BLL
 {
     public static class AutoMapperConfiguration
     {
+        /// <summary>
+        /// Create Maps for the BLL 
+        /// </summary>
         public static void CreateMap()
         {
             CreateMapArticle();
@@ -29,27 +32,42 @@ namespace ModuleBlog.BLL
             }
         }
 
+        /// <summary>
+        /// Create maps for "Articles"
+        /// </summary>
         private static void CreateMapArticle()
         {
             Mapper.CreateMap<ModuleBlog.DAL.Models.Article, ModuleBlog.BLL.Models.Article>();
             Mapper.CreateMap<ModuleBlog.BLL.Models.Article, ModuleBlog.DAL.Models.Article>();
         }
 
+        /// <summary>
+        /// Create maps for "Hashtags"
+        /// </summary>
         private static void CreateMapHashTag()
         {
             Mapper.CreateMap<ModuleBlog.DAL.Models.HashTagArticle, ModuleBlog.BLL.Models.HashTagArticle>();
         }
 
+        /// <summary>
+        /// Create maps for "Blog"
+        /// </summary>
         private static void CreateMapBlog()
         {
             Mapper.CreateMap<ModuleBlog.DAL.Models.Blog, ModuleBlog.BLL.Models.Blog>();
             Mapper.CreateMap<ModuleBlog.BLL.Models.Blog, ModuleBlog.DAL.Models.Blog>();
         }
 
+        /// <summary>
+        /// Create maps for "Categorie"
+        /// </summary>
         private static void CreateMapCategorie()
         {
             Mapper.CreateMap<ModuleBlog.DAL.Models.Categorie, ModuleBlog.BLL.Models.Categorie>();
         }
+        /// <summary>
+        /// Create maps for "Commentaire"
+        /// </summary>
 
         private static void CreateMapCommentaire()
         {
@@ -57,12 +75,18 @@ namespace ModuleBlog.BLL
             Mapper.CreateMap<ModuleBlog.BLL.Models.Commentaire, ModuleBlog.DAL.Models.Commentaire>();
         }
 
+        /// <summary>
+        /// Create maps for "Publicite"
+        /// </summary>
         private static void CreateMapPublicite()
         {
             Mapper.CreateMap<ModuleBlog.DAL.Models.Publicite, ModuleBlog.BLL.Models.Publicite>();
             Mapper.CreateMap<ModuleBlog.BLL.Models.Publicite, ModuleBlog.DAL.Models.Publicite>();
         }
 
+        /// <summary>
+        /// Create maps for "Theme"
+        /// </summary>
         private static void CreateMapTheme()
         {
             Mapper.CreateMap<ModuleBlog.DAL.Models.Theme, ModuleBlog.BLL.Models.Theme>();
